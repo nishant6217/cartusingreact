69e6abf6ae66f59e23bfd69bcc162f04f046f1ea
Nishant Kumar <me.nishant6217@gmail.com>
2021-06-24 20:28:40 +0530

    added props to cartite,

--- src/Cart.js
+++ src/Cart.js
@@ -1,14 +1,63 @@
 import React from 'react';
 import CartItem from './CartItem';
 class Cart extends React.Component{
+    constructor(){
+        super();
+        this .state ={
+            
+            products :[
+                {
+                    price :999,
+                    title :'Mobile Phone',
+                    qty :4,
+                    img : '',
+                    id :1
+                },
+                {
+                    price :99,
+                    title :'Watch',
+                    qty :1,
+                    img : '',
+                    id:2
+                },
+                {
+                    price :999,
+                    title :'Laptop',
+                    qty :4,
+                    img : '',
+                    id:3
+                }
+            ]
+        }
+        // this.increaseQuantity = this.increaseQuantity.bind(this)
+        // this.decreaseQuantity = this.decreaseQuantity.bind(this)
+        // this.testing();
+    }
 
     render(){
+        // const arr = [1,2,3,4,5];
+        const { products } = this.state;
         return (
             <div className="cart"> 
+               
+                {products.map((product)=>{
+                    return  <CartItem  
+                            product ={product} 
+                            key={product.id}
+                            // jsx={<h1>TEST</h1>}
+                            
+                            />
+                })}
+                
+                
+                
+                
+                {/* <CartItem/>
                 <CartItem/>
-                <CartItem/>
-                <CartItem/>
-                <CartItem/>
+                <CartItem/> */}
+                {/* {/* {arr.map((item)=>{
+                    return item+5;
+                })} */}
             </div>
         );
     }
